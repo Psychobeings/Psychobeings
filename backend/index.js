0a8f415f22212aa5a4f068d6a1d2ca5d3827add7
Rishabh Sharma <rishabhsharma919319@gmail.com>
2024-12-06 23:12:52 +0530

    add contect functionalty

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,23 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 import sessionRoute from './routes/sessionRoute.js';
+import EmailRouter from './routes/EmailRouter.js';
 import {Connection} from './config/connection.js';
 
 const app= express();
-
+// Enable CORS
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json())
 
 dotenv.config();
 Connection();
-
-app.use(cors());
-
+// app.use('/',(req, res, next)=>{
+//     res.send("this is from server side")
+//     next()
+// })
 app.use('/session-form', sessionRoute);
+app.use('/api/email', EmailRouter)
 
 app.listen(8080, ()=>{
     console.log("Server started!");
