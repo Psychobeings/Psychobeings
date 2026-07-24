fed4ac90535c319ee950f56fc1b1e5b6235b2806
Psychobeings <info.psychobeings@gmail.com>
2026-07-24 10:58:41 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Packages from "./pages/Packages";
+import Packages from './pages/Packages';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
 import SessionBookingForm from './components/SessionBookingForm';
 import ComingSoon from './components/ComingSoon';
 import { Toaster } from 'react-hot-toast';
-import TherapyDetails from './pages/TherapyDetails'
+import TherapyDetails from './pages/TherapyDetails';
+
 function App() {
-  const [isContactOpen, setIsContactOpen] = useState(false);
-
-  const openContactModal = () => setIsContactOpen(true);
-  const closeContactModal = () => setIsContactOpen(false);
-
   return (
     <Router>
       <div className="container mx-auto">
-        <Navbar onContactClick={openContactModal} />
+        <Navbar />
         <Toaster position="top-right" reverseOrder={false} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/services/:therapyId" element={<TherapyDetails/>} />
+          <Route path="/services/:therapyId" element={<TherapyDetails />} />
           <Route path="/packages" element={<Packages />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/individual-therapy" element={<SessionBookingForm />} />
           <Route path="/couples-therapy" element={<SessionBookingForm />} />
@@ -37,21 +32,6 @@ function App() {
         </Routes>
       </div>
       <Footer />
-
-      {/* Contact Modal */}
-      {/* {isContactOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-md">
-            <button 
-              className="absolute top-3 right-3 text-gray-500 hover:text-gray-700" 
-              onClick={closeContactModal}
-            >
-              &times;
-            </button> */}
-            {isContactOpen && <Contact onClose={closeContactModal} />}
-          {/* </div>
-        </div>
-      )} */}
     </Router>
   );
 }
