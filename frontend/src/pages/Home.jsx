c41b27b3f40776a8777dfd46d1d8ddcaca9e5b77
Psychobeings <info.psychobeings@gmail.com>
2026-07-29 18:08:19 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,9 +8,8 @@ import LanguageSupport from '../components/LanguageSupport';
 import FoundersNote from '../components/FoundersNote';
 import TherapyApproach from '../components/TherapyApproach';
 import CareAssessment from '../components/quick and interactive question';
-import Testimonials from '../components/testimonials';
 
-const homeSections = [Hero, WhatWeDo, LanguageSupport, FeatureSection, FoundersNote, TherapyApproach, CareAssessment, testimonials];
+const homeSections = [Hero, WhatWeDo, LanguageSupport, FeatureSection, FoundersNote, TherapyApproach, CareAssessment];
 
 const Home = () => {
   return (
