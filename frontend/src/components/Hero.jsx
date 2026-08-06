fe94f3adbf2b461433faee8a7be7eeea4c4339ef
Psychobeings <info.psychobeings@gmail.com>
2026-08-06 13:41:54 +0530

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Psychobeingslogo from '../assets/Psychobeingslogo.png';
+import Psychobeingslogo from '../assets/logo.png';
 
 const Hero = () => {
   return (
